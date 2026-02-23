4b0d9aa9ad4877a9cf9279591192f7a31c0b1852
ChaurasiaJayant <chaurasiajayant087@gmail.com>
2026-02-23 18:56:53 +0530

    Mock 2 Done

--- Basic_DSA/insertionSort.js
+++ Basic_DSA/insertionSort.js
@@ -5,8 +5,9 @@
 // then one by one we move to further elements and keep inserting them in their correct position
 
 function insertionSort(arr) {
-  // we assume that thie first element is sorted
-  let cnt = 0;
+  // we assume that the
+  //  first element is sorted
+
   for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
     let j = i - 1;
@@ -14,11 +15,10 @@ function insertionSort(arr) {
     while (j >= 0 && arr[j] > current) {
       arr[j + 1] = arr[j];
       j--;
-      cnt++;
     }
     arr[j + 1] = current;
   }
-  return cnt;
+  return arr;
 }
 
 console.log(insertionSort([5, 2, 4, 6, 1, 3]));
--- Basic_DSA/js2Mock.js
+++ Basic_DSA/js2Mock.js
@@ -331,5 +331,309 @@ function wordChars(sentence) {
 
 const users = [{ name: "Alice", address: { city: "Delhi" } }, { name: "Bob" }];
 
-console.log(users[0]?.address?.city);
-console.log(users[1]?.address?.city);
+// console.log(users[0]?.address?.city);
+// console.log(users[1]?.address?.city);
+
+// write a function to extract all the elements of nested arrays
+
+let arrr = [1, [2, 3, [4, 5, 6, [7, 8, 9]], 10]];
+// console.log(arr.flat());
+function flat(arr) {
+  let res = [];
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      res.push(...flat(item));
+    } else {
+      res.push(item);
+    }
+  }
+  return res;
+}
+// console.log(flat(arrr));
+
+const obj1 = {
+  name: "Jayant",
+  address: {
+    city: "Delhi",
+  },
+};
+
+let copy = { ...obj1 };
+// copy.address.city = "Gorakhpur";
+// console.log(copy);
+let deepCopy = structuredClone(obj1);
+deepCopy.address.city = "Gorakhpur";
+// console.log(deepCopy);
+
+let ar = [1, 2, 3, 8, 9, 4, 5, 6, 7, 8];
+function frequencyMap(arr) {
+  let map = new Map();
+  for (let num of arr) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
+    }
+  }
+  return map;
+}
+// console.log(frequencyMap(ar));
+
+// Trace through a binary search for target 7 in the
+// array [1, 3, 5, 7, 9, 11, 13]. Show each step with
+// left, right, and mid values.
+
+let arr1 = [1, 3, 5, 7, 9, 11, 13];
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (arr[mid] === target) return mid;
+    if (target < arr[mid]) right = mid - 1;
+    else left = mid + 1;
+  }
+  return -1;
+}
+// console.log(binarySearch(arr1, 9));
+
+function firstOccurence(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  let ans = -1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (arr[mid] === target) {
+      ans = mid;
+      right = mid - 1;
+    } else if (target < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return ans;
+}
+arr = [1, 2, 2, 2, 3, 4];
+// console.log(firstOccurence(arr, 2));
+
+const s = new Set([1, 2, 2, 3, 3, 3, 4]);
+// console.log(s.size);
+// console.log([...s]);
+
+// Write a function wordCount(str) that uses a Map to
+//  count occurrences of each word.
+
+// Input:  "the cat sat on the mat the cat"
+// Output: Map { 'the' => 3, 'cat' => 2, 'sat' => 1, 'on' => 1, 'mat' => 1 }
+
+function wordCount(str) {
+  let words = str.split(" ");
+  let map = new Map();
+  for (let ch of words) {
+    if (map.has(ch)) {
+      map.set(ch, map.get(ch) + 1);
+    } else {
+      map.set(ch, 1);
+    }
+  }
+  return map;
+}
+// console.log(wordCount("the cat sat on the mat the cat"));
+
+// Using Set, write a function intersection(a, b) that
+//  returns an array of elements present in both arrays.
+
+// Input: [1, 2, 3, 4], [3, 4, 5, 6]
+// Output: [3, 4]
+
+function intersection(a, b) {
+  let ans = [];
+  let set = new Set();
+
+  for (let num of a) {
+    if (!set.has(num)) {
+      set.add(num);
+    }
+  }
+  for (let num of b) {
+    if (set.has(num)) {
+      ans.push(num);
+    }
+  }
+  return ans;
+}
+let a = [1, 2, 3, 4];
+let b = [3, 4, 5, 6];
+// console.log(intersection(a, b));
+
+const map = new Map();
+const key = { id: 1 };
+
+map.set(key, "Alice");
+// console.log(map.get({ id: 1 })); // Line A
+// console.log(map.get(key)); // Line B
+
+// Write a recursive function factorial(n) that returns n!.
+
+// // factorial(5) => 120
+// // factorial(0) => 1
+
+function fact(n) {
+  if (n < 1) {
+    return 1;
+  }
+
+  return n * fact(n - 1);
+}
+// console.log(fact(5));
+
+// Write a recursive function fibonacci(n) that returns the nth Fibonacci number.
+
+// // fibonacci(6) => 8
+// // Sequence: 0, 1, 1, 2, 3, 5, 8,
+
+function fibonacci(n) {
+  if (n == 1) return 1;
+  if (n === 2) return 1;
+  if (n === 0) return;
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+// console.log(fibonacci(6));
+
+function sumArray(arr) {
+  if (arr.length === 0) return 0;
+  return arr[0] + sumArray(arr.slice(1));
+}
+// console.log(sumArray([1, 2, 3, 4]));
+
+function flattenDeep(arr) {
+  let res = [];
+  for (let el of arr) {
+    if (Array.isArray(el)) {
+      res.push(...flattenDeep(el));
+    } else {
+      res.push(el);
+    }
+  }
+  return res;
+}
+// console.log(flattenDeep([1, [2, [3, [4]], 5]]));
+
+// Implement the bubbleSort(arr) function.
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j + 1] < arr[j]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+// console.log(bubbleSort([6, 5, 4, 3, 2, 1]));
+
+// Implement selectionSort(arr). How does it differ from Bubble Sort?
+function selectionSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
+// console.log(selectionSort([5, 4, 3, 2, 1]));
+
+// Implement insertionSort(arr) and explain when it performs best.
+function insertionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && key < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+// console.log(insertionSort([7, 8, 6, 5, 4, 3, 2, 1, 9]));
+
+const nums = [10, 9, 2, 21, 3];
+nums.sort();
+// console.log(nums);
+
+function outer(x) {
+  return function inner(y) {
+    return x + y;
+  };
+}
+
+const add5 = outer(5);
+// console.log(add5(3));
+// console.log(add5(10));
+
+let numbers = [1, 2, 3, 4, 5];
+const squared = numbers.map((n) => n * n);
+// console.log(squared);
+// console.log(numbers);
+
+const words = ["apple", "banana", "kiwi", "mango", "fig"];
+const longWords = words.filter((w) => w.length > 4);
+// console.log(longWords);
+
+const numsss = [1, 2, 3, 4, 5];
+const sum = numsss.reduce((acc, curr) => acc + curr, 0);
+// console.log(sum);
+
+// Given the following array, use filter + map chaining to get the names of students who scored above 70.
+const students = [
+  { name: "Alice", score: 85 },
+  { name: "Bob", score: 60 },
+  { name: "Carol", score: 92 },
+  { name: "Dave", score: 70 },
+];
+
+function fltr(students) {
+  students
+    .filter((student) => {
+      return student.score > 70;
+    })
+    .map((el) => {
+      console.log(el.name);
+    });
+}
+// fltr(students);
+
+const orders = [
+  { id: 1, category: "food" },
+  { id: 2, category: "tech" },
+  { id: 3, category: "food" },
+  { id: 4, category: "clothes" },
+  { id: 5, category: "tech" },
+  { id: 6, category: "tech" },
+];
+function rdc(arr) {
+  let freq = arr.reduce((acc, word) => {
+    acc[word.category] = (acc[word.category] || 0) + 1;
+    return acc;
+  }, {});
+  console.log(freq);
+}
+rdc(orders);
